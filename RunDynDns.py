55ae5012e4dee2a49814e73b9c2885651297041c
Robert Bitel <lonecow@gmail.com>
2012-11-10 16:28:29 -0500

    Added Script that will run the Update process

---
+++ RunDynDns.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import getopt
+import sys
+from setDynDns import setDynDns
+from get_wan import getWan
+
+def usage():
+	print "RunDynDns.py\n\t-h\t--help\t\tdisplays this message\n\t-v\t--verbose\tdisplays verbose messages\n\t-f\t--force\t\tforces update of dyndns"
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hvf", ["help", "verbose", "force"])
+except getopt.GetoptError as err:
+	# print help information and exit:
+	print(err) # will print something like "option -a not recognized"
+	usage()
+	sys.exit(2)
+verbose = False
+Force = False
+for o, a in opts:
+	if o in ("-v", "--verbose"):
+		verbose = True
+	elif o in ("-h", "--help"):
+		usage()
+		sys.exit()
+	elif o in ("-f", "--force"):
+		Force = True
+	else:
+		assert False, "unhandled option"
+
+log_file = open('./RunDynDns_log.txt','a')
+
+getIp = getWan()
+
+if (getIp.isIpAddressValid() and getIp.hasIpAddressChanged()) or Force == True:
+	if getIp.isIpAddressValid() and not Force:
+		update = setDynDns(getIp.getIpAddress())
+		log_file.write("Sending Out Change in IP OLD[%s]:New[%s] Ret Code [%s]\n" % (getIp.getLastIPAddress(), getIp.getIpAddress(), update.getReturnCode()))
+	elif getIp.isIpAddressValid() and Force:
+		update = setDynDns(getIp.getIpAddress())
+		log_file.write("Sending Out Change in IP OLD[%s]:New[%s] Ret Code [%s] By Force\n" % (getIp.getLastIPAddress(), getIp.getIpAddress(), update.getReturnCode()))
+	elif Force:
+		update = setDynDns(getIp.getLastIPAddress())
+		log_file.write("Sending Out Change in IP OLD[%s]:New[%s] Ret Code [%s] By Force\n" % (getIp.getLastIPAddress(), getIp.getIpAddress(), update.getReturnCode()))
+		
+
+log_file.close()
